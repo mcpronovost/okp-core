e985ddfb34d2d3e09f9965f13764356858f7bb13
M-C Pronovost <mc@nixa.ca>
2025-01-27 16:12:08 -0500

    move

--- backend/okp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class UsersConfig(AppConfig):
-    name = "okp.users"
-    label = "users"
-    verbose_name = _("Authentication & Authorization")
