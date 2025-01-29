baf197e4590d8e52a05cb54d7146f459d714ef63
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-01-28 23:20:20 -0500

    clean

--- README.md
+++
@@ -1,2 +0,0 @@
-# okp-core
-by mcpronovost
--- okp/users/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class OkpUsersConfig(AppConfig):
-    name = "okp.users"
-    label = "okp_users"
-    verbose_name = _("Authentication & Authorization")
