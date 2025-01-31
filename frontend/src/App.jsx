8bb4b8d45d2c590664ccb00e505ad47988525a53
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-01-30 21:42:43 -0500

    add axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleLogin = () => {
+    axios.post("http://localhost:8000/api/v1/auth/login/", {
+      username: "mc",
+      password: "1"
+    }, {
+      headers: {
+        "Accept-CH": "Sec-CH-UA-Platform"
+      }
+    })
+    .then((response) => {
+      console.log(">> response : ", response)
+    })
+    .catch((error) => {
+      console.log(">> error : ", error)
+    })
+  }
 
   return (
     <>
@@ -20,6 +38,9 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button onClick={handleLogin}>
+          Login
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
