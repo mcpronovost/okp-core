26d7dce957a346f7f7c8a37babb89a28ca7111c8
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-08 11:31:47 -0500

    Simplify Vite environment variable loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default ({ mode }) => {
-  const rootPath = path.resolve(__dirname, "..");
-  const env = loadEnv(mode, rootPath, "");
-  process.env = {...process.env, ...env};
+  loadEnv(mode, "../", "", true);
   return defineConfig({
     plugins: [react()],
     base: "/",
