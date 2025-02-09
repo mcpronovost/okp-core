9b37460dfc2d99f71c261c671de8d13e37b11378
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-09 15:31:04 -0500

    Implement @mcpronovost/okp-router for improved routing and language handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,72 +1,19 @@
 import { useEffect, useState } from "react";
-import { findRoute } from "@/services/router";
-import { LANGUAGES } from "@/services/utils/constants";
+import { getView } from "@mcpronovost/okp-router";
 import Loading from "@/views/Loading";
 
-const views = import.meta.glob("./views/**/*.jsx", {
-    eager: false,
-});
-
 function App() {
-  const lang = document.documentElement.lang;
-  const path = window.location.pathname
-  const [, langCode, ...uriParts] = path.split(/^\/([a-z]{2})\//);
-  const uri = uriParts.join("/");
-
-  // If the language is not supported, redirect to the default language
-  if (!langCode || !LANGUAGES.includes(langCode)) {
-    window.location.href = `/${lang}/${uri || path.replace(/^\//, "")}`;
-  }
-
   const [OkpView, setOkpView] = useState(null);
   const [viewProps, setViewProps] = useState({});
   const [viewParams, setViewParams] = useState({});
 
   const doSetView = async () => {
-    const route = findRoute(uri, lang);
+    const { viewModule, props, params } = await getView();
 
-    if (!route) {
-      // If the route is not found, redirect to the 404 page
-      return window.location.href = `/${lang}/404`;
-    }
+    setViewProps(props || {});
+    setViewParams(params || {});
 
-    try {
-      const [_, { view, auth, props, params }] = route;
-      const viewPath = `./views/${view}.jsx`;
-
-      if (!views[viewPath]) {
-        // If the view is not found, redirect to the 404 page
-        return window.location.href = `/${lang}/404`;
-      }
-
-      const viewModule = await views[viewPath]();
-
-      /**
-       * Set the view props and params
-       */
-      setViewProps(props || {});
-      setViewParams(params || {});
-
-      /**
-       * Get the user data
-       */
-      // const me = await usersApi.me();
-
-      /**
-       * Redirect to 403 if the route is protected and the user is not authenticated
-       */
-      // if (auth && !me) {
-      //   return Astro.redirect(`/${lang}/403`);
-      // }
-
-      /**
-       * Set the view component
-       */
-      setOkpView(() => viewModule.default);
-    } catch (error) {
-      console.error("Failed to load view:", error);
-      // window.location.href = `/${lang}/404`;
-    }
+    setOkpView(() => viewModule.default);
   };
 
   useEffect(() => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,19 @@
 import "@/assets/style/main.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { initRouter } from "@mcpronovost/okp-router";
 import App from "./App.jsx";
+
+initRouter({
+  defaultLang: import.meta.env.VITE_LANG,
+  supportedLangs: import.meta.env.VITE_LANGS.split(","),
+  routeModules: import.meta.glob("./_services/routes/**/*.js", {
+    eager: true,
+  }),
+  views: import.meta.glob("./views/**/*.jsx", {
+    eager: false,
+  })
+});
 
 createRoot(document.getElementById("okp")).render(
   <StrictMode>
