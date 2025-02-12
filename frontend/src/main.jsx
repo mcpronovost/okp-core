6c6af884f7eb242b3d407a8f38cc870baec8cb96
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-11 21:10:09 -0500

    Add internationalization support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,15 @@
 import "@/assets/style/main.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { initI18n } from "@mcpronovost/okp-i18n";
 import { initRouter } from "@mcpronovost/okp-router";
 import App from "./App.jsx";
+
+await initI18n({
+  defaultLang: import.meta.env.VITE_LANG,
+  supportedLangs: import.meta.env.VITE_LANGS.split(","),
+  localesPath: "/src/_services/locales",
+});
 
 initRouter({
   defaultLang: import.meta.env.VITE_LANG,
