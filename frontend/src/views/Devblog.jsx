2b2f282d2255f4a0cca57ebe891a74d5ab3d03db
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-11 23:40:23 -0500

    tests

--- frontend/src/views/Devblog.jsx
+++ frontend/src/views/Devblog.jsx
@@ -1,6 +1,17 @@
-function Devblog() {
-  return <div>Devblog</div>;
+import { t } from "@mcpronovost/okp-i18n";
+import { r } from "@mcpronovost/okp-router";
+
+export default function Devblog() {
+  const handleClick = (url) => {
+    window.location.href = url;
+  };
+
+  return (
+    <div>
+      <h1>Devblog</h1>
+      <p>{t("Hello")}</p>
+      <button onClick={() => handleClick(r("devblog", "en", "fr"))}>FR</button>
+      <button onClick={() => handleClick(r("devblog", "en", "en"))}>EN</button>
+    </div>
+  );
 }
-
-export default Devblog;
-
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,5 +1,13 @@
-function Home() {
-  return <div>Home</div>;
-}
+import { t } from "@mcpronovost/okp-i18n";
+import { r } from "@mcpronovost/okp-router";
 
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>{t("Hello")}</p>
+      <a href={r("home", "en", "fr")}>Go to Home FR</a>
+      <a href={r("home", "en", "en")}>Go to Home EN</a>
+    </div>
+  );
+}
