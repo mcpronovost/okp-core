922412078e3343f0c7777db4de68fde1a268d97b
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-14 20:04:59 -0500

    Normalize line endings

--- frontend/src/pages/Devblog.jsx
+++ frontend/src/pages/Devblog.jsx
@@ -1,17 +1,18 @@
 import { t } from "@mcpronovost/okp-i18n";
-import { r } from "@mcpronovost/okp-router";
+import { switchLang } from "@mcpronovost/okp-router";
 
 export default function Devblog() {
-  const handleClick = (url) => {
-    window.location.href = url;
+  
+  const handleSwitchLang = (lang) => {
+    window.location.href = switchLang(lang);
   };
 
   return (
     <div>
       <h1>Devblog</h1>
       <p>{t("Hello")}</p>
-      <button onClick={() => handleClick(r("devblog", "en", "fr"))}>FR</button>
-      <button onClick={() => handleClick(r("devblog", "en", "en"))}>EN</button>
+      <button onClick={() => handleSwitchLang("fr")}>FR</button>
+      <button onClick={() => handleSwitchLang("en")}>EN</button>
     </div>
   );
 }
